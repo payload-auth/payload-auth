3e3e1068b1b8dd31b0394c835d9ec081c9237592
ForrestDevs <luke.gannon@me.com>
2025-04-04 14:45:58 -0400

    chore(clerk): remove test code

--- examples/clerk/src/payload/collections/users.ts
+++ examples/clerk/src/payload/collections/users.ts
@@ -3,9 +3,5 @@ import type { CollectionConfig } from 'payload'
 export const usersCollection: CollectionConfig = {
   slug: 'users',
   auth: true,
-  fields: [
-    { name: 'name', type: 'text' },
-    { name: 'packageField', type: 'text', admin: { components: { afterInput: [{ path: 'payload-auth/clerk/admin/ui#PackageTestField' }] } } },
-    { name: 'localField', type: 'text', admin: { components: { afterInput: [{ path: '@/payload/components/local-field#LocalField' }] } } }
-  ]
+  fields: [{ name: 'name', type: 'text' }]
 }
