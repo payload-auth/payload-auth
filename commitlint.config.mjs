7472f349e3dd22165743d85c14c8237bbb5f1a4c
ForrestDevs <luke.gannon@me.com>
2025-04-17 20:40:28 -0400

    chore(monorepo): add commitlint config

---
+++ commitlint.config.mjs
@@ -0,0 +1,47 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { maxLineLength } from "@commitlint/ensure";
+
+const bodyMaxLineLength = 100;
+
+const validateBodyMaxLengthIgnoringDeps = (parsedCommit) => {
+  const { type, scope, body } = parsedCommit;
+  const isDepsCommit =
+    type === "chore" && (scope === "deps" || scope === "deps-dev");
+
+  return [
+    isDepsCommit || !body || maxLineLength(body, bodyMaxLineLength),
+    `body's lines must not be longer than ${bodyMaxLineLength}`,
+  ];
+};
+
+export default {
+  extends: ["@commitlint/config-conventional"],
+  plugins: ["commitlint-plugin-function-rules"],
+  rules: {
+    "body-max-line-length": [0],
+    "function-rules/body-max-line-length": [
+      2,
+      "always",
+      validateBodyMaxLengthIgnoringDeps,
+    ],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+        "ui",
+        "examples",
+      ],
+    ],
+  },
+};
